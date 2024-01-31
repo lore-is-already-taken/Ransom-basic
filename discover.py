f03d65f0e41cfe248437173ddab6d7c5177790b9
Klithik <santi17461@gmail.com>
2024-01-31 16:12:16 -0300

    encontrar archivos

--- discover.py
+++ discover.py
@@ -1,4 +1,14 @@
-extensiones = {}
+import os
+import fnmatch
+
+extensiones = ["pdf","jpg"]
 origen = "/"
-def encuentra_archivos(extensiones,origen):
-    return false
+def encuentra_archivos():
+    archivos_encontrados = []
+    for ruta_actual, directorios, archivos in os.walk(origen):
+        for archivo in archivos:
+            for extension in extensiones:
+                if fnmatch.fnmatch(archivo, f'*.{extension}'):
+                    archivos_encontrados.append(os.path.join(ruta_actual, archivo))
+
+    return archivos_encontrados
