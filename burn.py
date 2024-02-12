a5fadf580eb229f172c043b4252f7bcc4c9f383d
Klithik <santi17461@gmail.com>
2024-02-12 16:27:01 -0300

    eliminados comentarios

--- burn.py
+++ burn.py
@@ -32,11 +32,9 @@ def encrypt(f):
     recipient_key = RSA.import_key(open("seal.pem").read())
     session_key = get_random_bytes(32)
 
-    # Encrypt the session key with the public RSA key
     cipher_rsa = PKCS1_OAEP.new(recipient_key)
     enc_session_key = cipher_rsa.encrypt(session_key)
 
-    # Encrypt the data with the AES session key
     cipher_aes = AES.new(session_key, AES.MODE_EAX)
     ciphertext, tag = cipher_aes.encrypt_and_digest(data)
     
